2aeca06075e35911853b4c93c2acf7ddf6616a6c
mjmdurand <mjm.durand@hotmail.fr>
2022-07-23 14:14:29 +0200

    adding sudo check

--- nextcloud-php8.0-ubuntu22.04.sh
+++ nextcloud-php8.0-ubuntu22.04.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
 
-HOME_DIR=~
+HOME_DIR=/home/$SUDO_USER/
 IPCONF=/etc/netplan/static.yaml
+
+echo -e "\n\e[1;44mVerifications.\e[0m"
+#Checking if script is launched with sudo/root
+if [  "$USER" != "root" ]
+then
+echo -e "\e[1;31mCe script doit être executé en tant que root/sudo, arrêt du script.\e[0m"
+exit
+fi
 
 #moving to user's directory
 cd ${HOME_DIR}
@@ -30,19 +38,19 @@ network:
           via: 0.0.0.0
           scope: link' > $IPCONF
 
-sudo mv /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.old
-sudo netplan apply
+mv /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.old
+netplan apply
 fi
 
 # update system 
 echo -e "\e[1;34mUpdating the system.\e[0m"
-sudo apt update -q && sudo apt upgrade -y -q
+apt update -q && apt upgrade -y -q
 echo -e "\e[1;32mSystem updated.\e[0m"
 echo ""
 
 # install nginx, php, certbot, redis, mariaDB, ufw, unzip, fail2ban, portsentry
 echo -e "\e[1;34mInstalling NGINX web server.\e[0m"
-sudo apt-get -y -q install nginx php-fpm php-cli php-json php-curl php-imap php-gd php-mysql php-xml php-zip php-intl php-imagick php-mbstring php-bcmath php-gmp software-properties-common certbot python3-certbot-nginx php-apcu redis-server php-redis mariadb-server mariadb-client ufw portsentry unzip fail2ban
+apt-get -y -q install nginx php-fpm php-cli php-json php-curl php-imap php-gd php-mysql php-xml php-zip php-intl php-imagick php-mbstring php-bcmath php-gmp software-properties-common certbot python3-certbot-nginx php-apcu redis-server php-redis mariadb-server mariadb-client ufw portsentry unzip fail2ban
 echo -e "\e[1;32mInstallation done.\e[0m"
 echo ""
 
@@ -50,29 +58,29 @@ echo ""
 echo -e "\e[1;34mUpdtating nginx.conf file\e[0m"
 PROCESSORS=$(grep processor /proc/cpuinfo | wc -l)
 
-sudo sed -i 's/worker_processes auto;/worker_processes '${PROCESSORS}';/g' /etc/nginx/nginx.conf
-sudo sed -i 's/# server_tokens off;/server_tokens off;/g' /etc/nginx/nginx.conf
+sed -i 's/worker_processes auto;/worker_processes '${PROCESSORS}';/g' /etc/nginx/nginx.conf
+sed -i 's/# server_tokens off;/server_tokens off;/g' /etc/nginx/nginx.conf
 
 echo -e "\e[1;32mNginx.conf file updated.\e[0m"
 echo ""
 
 # Creating web folder
 echo -e "\e[1;34mCreating /home/www-data\e[0m"
-sudo mkdir /home/www-data/
-sudo chown -R ${USER}:www-data /home/www-data
-sudo chmod -R 770 /home/www-data
+mkdir /home/www-data/
+chown -R ${USER}:www-data /home/www-data
+chmod -R 770 /home/www-data
 echo -e "\e[1;32mDone.\e[0m"
 echo ""
 
 #Installing Nextcloud
 #Adding php 8.0
-sudo add-apt-repository ppa:ondrej/php
-sudo apt update && sudo apt upgrade && sudo apt install -y -q php8.0-fpm php8.0-common php8.0-mysql php8.0-xml php8.0-xmlrpc php8.0-curl php8.0-gd php8.0-imagick php8.0-cli php8.0-dev php8.0-imap php8.0-mbstring php8.0-opcache php8.0-soap php8.0-zip
+add-apt-repository ppa:ondrej/php
+apt update && apt upgrade && apt install -y -q php8.0-fpm php8.0-common php8.0-mysql php8.0-xml php8.0-xmlrpc php8.0-curl php8.0-gd php8.0-imagick php8.0-cli php8.0-dev php8.0-imap php8.0-mbstring php8.0-opcache php8.0-soap php8.0-zip
 
-sudo wget https://download.nextcloud.com/server/releases/latest.tar.bz2
-sudo wget https://download.nextcloud.com/server/releases/latest.tar.bz2.sha256
-sudo wget https://download.nextcloud.com/server/releases/latest.tar.bz2.asc
-sudo wget https://nextcloud.com/nextcloud.asc
+wget https://download.nextcloud.com/server/releases/latest.tar.bz2
+wget https://download.nextcloud.com/server/releases/latest.tar.bz2.sha256
+wget https://download.nextcloud.com/server/releases/latest.tar.bz2.asc
+wget https://nextcloud.com/nextcloud.asc
 
 #check SHA256
 echo -e "\e[1;34mChecking SHA256\e[0m"
@@ -89,29 +97,29 @@ echo ""
 
 #Uncompressing Nextcloud
 echo -e "\e[1;34mUncompressing nextcloud\e[0m"
-sudo tar -xvf latest.tar.bz2 -C /home/www-data/
+tar -xvf latest.tar.bz2 -C /home/www-data/
 
 #Cleaning useless files
 echo -e "\e[1;34mRemoving compressed files\e[0m"
-sudo rm latest.tar.bz2* nextcloud.asc
+rm latest.tar.bz2* nextcloud.asc
 
 #Adding Nextcloud User
 echo -e "\e[1;34mAdding Nextcloud user\e[0m"
-sudo adduser nextcloud
-sudo adduser nextcloud www-data
+adduser nextcloud
+adduser nextcloud www-data
 
 #Setting Permissions
 echo -e "\e[1;34mSetting permissions\e[0m"
-sudo chown -R nextcloud:www-data /home/www-data/nextcloud
-sudo chmod -R 770 /home/www-data
+chown -R nextcloud:www-data /home/www-data/nextcloud
+chmod -R 770 /home/www-data
 
 
 #Setting pm.max_children parameter for Nextcloud Pool
 echo -e "\e[1;34mCalculating pm.max_children\e[0m"
-sudo systemctl stop php8.0-fpm.service
+systemctl stop php8.0-fpm.service
 free -m
 echo "-----------------------"
-sudo systemctl start php8.0-fpm.service && ps --no-headers -o "rss,cmd" -C php-fpm8.0 | awk '{ sum+=$1 } END { printf ("%d%s\n", sum/NR/1024,"M") }'
+systemctl start php8.0-fpm.service && ps --no-headers -o "rss,cmd" -C php-fpm8.0 | awk '{ sum+=$1 } END { printf ("%d%s\n", sum/NR/1024,"M") }'
 echo "-----------------------"
 echo 'To set value, use this : '
 echo 'RAM you wanna allocate / number above = pm.max_children value' 
@@ -121,7 +129,7 @@ echo ""
 
 #Creating Nextcloud Pool
 echo -e "\e[1;34mCreating nextcloud pool\e[0m"
-sudo echo '
+echo '
 [nextcloud]
 listen = /var/run/nextcloud.sock
 
@@ -152,16 +160,16 @@ echo -e "\e[41m[Service]
 UMask=0027\e[0m"
 read -n 1 -s -r -p "Press any key to continue."
 echo ""
-sudo systemctl start php8.0-fpm.service
-sudo systemctl edit php8.0-fpm.service
-sudo systemctl reenable php8.0-fpm.service
+systemctl start php8.0-fpm.service
+systemctl edit php8.0-fpm.service
+systemctl reenable php8.0-fpm.service
 
 #MariaDB configuration
 echo -e "\e[1;34mMariaDB configuration\e[0m"
 echo -e "\e[1;41mPlease use differents passwords for root and nextcloud user\e[0m"
 read -n 1 -s -r -p "Press any key to continue."
 echo ""
-sudo mysql_secure_installation
+mysql_secure_installation
 
 
 #Creating new database
@@ -171,26 +179,26 @@ read -p "Nextcloud database user : " newnextclouddbuser
 echo -e "\e[1;41mPlease do not use the same password for nextcloud UNIX user and nextcloud SQL user\e[0m"
 read -s -p "Nextcloud database user password : " newnextclouddbpasswd
 
-sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${newnextclouddbname} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${newnextclouddbname} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 echo -e "\e[1;32m${newnextclouddbname} database created.\e[0m"
 
-sudo mysql -u root -e "CREATE USER IF NOT EXISTS "${newnextclouddbuser}"@"localhost";"
+mysql -u root -e "CREATE USER IF NOT EXISTS "${newnextclouddbuser}"@"localhost";"
 echo -e "\e[1;32m${newnextclouddbuser} user created.\e[0m"
 
-sudo mysql -u root -e "SET password FOR "${newnextclouddbuser}"@"localhost" = password('${newnextclouddbpasswd}');"
+mysql -u root -e "SET password FOR "${newnextclouddbuser}"@"localhost" = password('${newnextclouddbpasswd}');"
 echo -e "\e[1;32mPassword set.\e[0m"
 
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON ${newnextclouddbname}.* TO "${newnextclouddbuser}"@"localhost" IDENTIFIED BY '${newnextclouddbpasswd}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${newnextclouddbname}.* TO "${newnextclouddbuser}"@"localhost" IDENTIFIED BY '${newnextclouddbpasswd}';"
 echo -e "\e[1;32mPermissions set.\e[0m"
 
-sudo mysql -u root -e "FLUSH PRIVILEGES;" 
+mysql -u root -e "FLUSH PRIVILEGES;" 
 
 
 #Virtual Host
 echo -e "\e[1;34mCreating nginx virtual host file\e[0m"
 read -p "Domain name for the nexcloud website : " nextclouddomain
 
-sudo echo "
+echo "
 upstream php-nextcloud {
     server                        unix:/var/run/nextcloud.sock;
 }
@@ -302,68 +310,68 @@ server {
 
 #Enable virtual host
 echo -e "\e[1;34mCreating nginx virtual host file\e[0m"
-sudo ln -s /etc/nginx/sites-available/nextcloud.conf /etc/nginx/sites-enabled/nextcloud.conf
+ln -s /etc/nginx/sites-available/nextcloud.conf /etc/nginx/sites-enabled/nextcloud.conf
 
 #Generating SSL certs
 echo -e "\e[1;34mCreating SSL certs\e[0m"
-sudo certbot --nginx
+certbot --nginx
 
 #Testing auto renew
 echo -e "\e[1;34mTesting auto renew SSL certs\e[0m"
-sudo certbot renew --dry-run
+certbot renew --dry-run
 
-#sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
-#sudo chmod 600 /etc/ssl/certs/dhparam.pem
+#openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
+#chmod 600 /etc/ssl/certs/dhparam.pem
 
 
 #Updating virtual host
 echo -e "\e[1;34mUpdating virtual host (http2)\e[0m"
-sudo sed -i 's/443 ssl/443 ssl http2/g' /etc/nginx/sites-available/nextcloud.conf
+sed -i 's/443 ssl/443 ssl http2/g' /etc/nginx/sites-available/nextcloud.conf
 
-sudo systemctl restart nginx.service
-sudo systemctl restart php8.0-fpm.service
+systemctl restart nginx.service
+systemctl restart php8.0-fpm.service
 
 echo -e "\e[41mGo at https://${nextclouddomain} and finish installation first.\e[0m"
 read -n 1 -s -r -p "Once configuration is done, press any key to continue."
 #Updating virtual host
 echo -e "\e[1;34mUpdating virtual host (timeout)\e[0m"
-sudo sed -i '/fastcgi_read_timeout      3600;/d' /etc/nginx/sites-available/nextcloud.conf
+sed -i '/fastcgi_read_timeout      3600;/d' /etc/nginx/sites-available/nextcloud.conf
 
 #Updating php8.0-fpm
 echo -e "\e[1;34mUpdating php8.0-fpm (timeout)\e[0m"
-sudo sed -i '/request_terminate_timeout = 3600/d' /etc/php/8.0/fpm/pool.d/nextcloud.conf
+sed -i '/request_terminate_timeout = 3600/d' /etc/php/8.0/fpm/pool.d/nextcloud.conf
 
 
 #Opcache
 echo -e "\e[1;34mUpdating php8.0-fpm (opcache)\e[0m"
-sudo sed -i 's/;opcache.enable=1/opcache.enable=1/g' /etc/php/8.0/fpm/php.ini
-sudo sed -i 's/;opcache.enable_cli=0/opcache.enable_cli=1/g' /etc/php/8.0/fpm/php.ini
-sudo sed -i 's/;opcache.memory_consumption=128/opcache.memory_consumption=128/g' /etc/php/8.0/fpm/php.ini
-sudo sed -i 's/;opcache.interned_strings_buffer=8/opcache.interned_strings_buffer=32/g' /etc/php/8.0/fpm/php.ini
-sudo sed -i 's/;opcache.max_accelerated_files=10000/opcache.max_accelerated_files=10000/g' /etc/php/8.0/fpm/php.ini
-sudo sed -i 's/;opcache.revalidate_freq=2/opcache.revalidate_freq=1/g' /etc/php/8.0/fpm/php.ini
-sudo sed -i 's/;opcache.save_comments=1/opcache.save_comments=1/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/;opcache.enable=1/opcache.enable=1/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/;opcache.enable_cli=0/opcache.enable_cli=1/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/;opcache.memory_consumption=128/opcache.memory_consumption=128/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/;opcache.interned_strings_buffer=8/opcache.interned_strings_buffer=32/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/;opcache.max_accelerated_files=10000/opcache.max_accelerated_files=10000/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/;opcache.revalidate_freq=2/opcache.revalidate_freq=1/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/;opcache.save_comments=1/opcache.save_comments=1/g' /etc/php/8.0/fpm/php.ini
 
 
 
 #Adding APCu and redis
-sudo sed -i "s/'installed' => true,/'installed' => true,\n  'memcache.local' => '\\\OC\\\Memcache\\\APCu',\n  'memcache.locking' => '\\\OC\\\Memcache\\\Redis',\n  'redis' => array(\n    'host' => 'localhost',\n    'port' => 6379,\n  ),\nfail2ban/g" /home/www-data/nextcloud/config/config.php
-sudo echo "apc.enable_cli=1" >> /etc/php/8.0/mods-available/apcu.ini
-sudo systemctl restart php8.0-fpm.service
+sed -i "s/'installed' => true,/'installed' => true,\n  'memcache.local' => '\\\OC\\\Memcache\\\APCu',\n  'memcache.locking' => '\\\OC\\\Memcache\\\Redis',\n  'redis' => array(\n    'host' => 'localhost',\n    'port' => 6379,\n  ),\nfail2ban/g" /home/www-data/nextcloud/config/config.php
+echo "apc.enable_cli=1" >> /etc/php/8.0/mods-available/apcu.ini
+systemctl restart php8.0-fpm.service
 
 #Fail2Ban configuration
-sudo mkdir /var/log/nextcloud
-sudo chown nextcloud:nextcloud /var/log/nextcloud
-sudo sed -i "s/fail2ban/  'loglevel' => 2,\n  'logtimezone' => 'Europe\/Paris',\n  'logfile' => '\/var\/log\/nextcloud\/nextcloud.log',\n  'log_rotate_size' => '104857600'/g" /home/www-data/nextcloud/config/config.php
+mkdir /var/log/nextcloud
+chown nextcloud:nextcloud /var/log/nextcloud
+sed -i "s/fail2ban/  'loglevel' => 2,\n  'logtimezone' => 'Europe\/Paris',\n  'logfile' => '\/var\/log\/nextcloud\/nextcloud.log',\n  'log_rotate_size' => '104857600'/g" /home/www-data/nextcloud/config/config.php
 
-sudo echo '
+echo '
 [Definition]
 failregex=^{"reqId":".*","remoteAddr":".*","app":"core","message":"Login failed: ''.*'' \(Remote IP: ''<HOST>''\)","level":2,"time":".*"}$
           ^{"reqId":".*","level":2,"time":".*","remoteAddr":".*","user,:".*","app":"no app in context".*","method":".*","message":"Login failed: ''.*'' \(Remote IP: ''<HOST>''\)".*}$
           ^{"reqId":".*","level":2,"time":".*","remoteAddr":".*","user":".*","app":".*","method":".*","url":".*","message":"Login failed: .* \(Remote IP: <HOST>\).*}$' >> /etc/fail2ban/filter.d/nextcloud.conf
 
 
-sudo echo '
+echo '
 [DEFAULT]
 ignoreip = 127.0.0.1/8
 ignorecommand =
@@ -539,12 +547,12 @@ maxretry = 3
 bantime = 3600
 logpath = /var/log/nextcloud/nextcloud.log' >> /etc/fail2ban/jail.local
 
-sudo echo '
+echo '
 [Definition]
 failregex = <HOST> - - [.*?] ".*?" 4(0[0-9]|1[0-5])
 ignoreregex =' >> /etc/fail2ban/filter.d/nginx-404.conf
 
-sudo echo '
+echo '
 # Proxy filter /etc/fail2ban/filter.d/nginx-proxy.conf:
 #
 # Block IPs trying to use server as proxy.
@@ -556,7 +564,7 @@ sudo echo '
 failregex = ^<HOST> -.*GET http.*
 ignoreregex =' >> /etc/fail2ban/filter.d/nginx-proxy.conf
 
-sudo echo '
+echo '
 # Noscript filter /etc/fail2ban/filter.d/nginx-noscript.conf:
 #
 # Block IPs trying to execute scripts such as .php, .pl, .exe and other funny scripts.
@@ -568,7 +576,7 @@ sudo echo '
 failregex = ^<HOST> -.*GET.*(\.php|\.asp|\.exe|\.pl|\.cgi|\scgi)
 ignoreregex =' >> /etc/fail2ban/filter.d/nginx-noscript.conf
 
-sudo echo '
+echo '
 # Auth filter /etc/fail2ban/filter.d/nginx-auth.conf:
 #
 # Blocks IPs that fail to authenticate using basic authentication
@@ -580,7 +588,7 @@ authentication.*client: <HOST>
             user .* password mismatch.*client: <HOST>
 ignoreregex =' >> /etc/fail2ban/filter.d/nginx-auth.conf
 
-sudo echo '
+echo '
 # Login filter /etc/fail2ban/filter.d/nginx-login.conf:
 #
 # Blocks IPs that fail to authenticate
@@ -591,7 +599,7 @@ sudo echo '
 failregex = ^<HOST> -.*POST /sessions HTTP/1\.." 200
 ignoreregex =' >> /etc/fail2ban/filter.d/nginx-login.conf
 
-sudo echo '
+echo '
 # Fail2Ban filter for postfix authentication failures
 #
 
@@ -607,7 +615,7 @@ failregex = ^%(__prefix_line)swarning: [-._\w]+\[<HOST>\]: SASL (?:LOGIN|PLAIN|>
 
 # Author: Yaroslav Halchenko' >> /etc/fail2ban/filter.d/postfix-sasl.conf
 
-sudo echo '
+echo '
 # Fail2Ban ssh filter for at attempted exploit
 #
 # The regex here also relates to a exploit:
@@ -634,51 +642,51 @@ ignoreregex =
 
 # Author: Yaroslav Halchenko' >> /etc/fail2ban/filter.d/sshd-ddos.conf
 
-sudo echo '#Postifix jail log' >> /var/log/mail.log
-sudo echo '#Xinetd-fail log' >> /var/log/daemon.log
+echo '#Postifix jail log' >> /var/log/mail.log
+echo '#Xinetd-fail log' >> /var/log/daemon.log
 
-sudo systemctl restart fail2ban.service
+systemctl restart fail2ban.service
 
 #set php memory
-sudo sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/8.0/fpm/php.ini
+sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/8.0/fpm/php.ini
 
-sudo chown -R www-data:www-data /home/www-data
-sudo chown -R nextcloud:www-data /home/www-data/nextcloud
-sudo systemctl restart php8.0-fpm.service
+chown -R www-data:www-data /home/www-data
+chown -R nextcloud:www-data /home/www-data/nextcloud
+systemctl restart php8.0-fpm.service
 
 #patching database
-sudo -u nextcloud php8.0 /home/www-data/nextcloud/occ db:add-missing-indices
-sudo -u nextcloud php8.0 /home/www-data/nextcloud/occ db:convert-filecache-bigint
+-u nextcloud php8.0 /home/www-data/nextcloud/occ db:add-missing-indices
+-u nextcloud php8.0 /home/www-data/nextcloud/occ db:convert-filecache-bigint
 
 #portsentry configuration
-sudo sed -i 's/BLOCK_UDP="0"/BLOCK_UDP="1"/g' /etc/portsentry/portsentry.conf
-sudo sed -i 's/BLOCK_TCP="0"/BLOCK_TCP="1"/g' /etc/portsentry/portsentry.conf
-sudo sed -i 's/\/usr\/share\/doc\/portsentry\/examples\//\/usr\/share\/doc\/portsentry\/examples\/ \nKILL_RUN_CMD="\/sbin\/iptables -I INPUT -s $TARGET$ -j DROP \&\& \/sbin\/iptables -I INPUT -s $TARGET$ -m limit --limit 3\/minute --limit-burst 5 -j LOG --log-level debub --log-prefix ''Portsentry: dropping: ''"/g' /etc/portsentry/portsentry.conf
-sudo sed -i 's/TCP_MODE="tcp"/TCP_MODE="atcp"/g' /etc/default/portsentry
-sudo sed -i 's/UDP_MODE="udp"/UDP_MODE="audp"/g' /etc/default/portsentry
-sudo service portsentry restart
+sed -i 's/BLOCK_UDP="0"/BLOCK_UDP="1"/g' /etc/portsentry/portsentry.conf
+sed -i 's/BLOCK_TCP="0"/BLOCK_TCP="1"/g' /etc/portsentry/portsentry.conf
+sed -i 's/\/usr\/share\/doc\/portsentry\/examples\//\/usr\/share\/doc\/portsentry\/examples\/ \nKILL_RUN_CMD="\/sbin\/iptables -I INPUT -s $TARGET$ -j DROP \&\& \/sbin\/iptables -I INPUT -s $TARGET$ -m limit --limit 3\/minute --limit-burst 5 -j LOG --log-level debub --log-prefix ''Portsentry: dropping: ''"/g' /etc/portsentry/portsentry.conf
+sed -i 's/TCP_MODE="tcp"/TCP_MODE="atcp"/g' /etc/default/portsentry
+sed -i 's/UDP_MODE="udp"/UDP_MODE="audp"/g' /etc/default/portsentry
+service portsentry restart
 
 #updating SSH port
 read -p "Please choose the new SSH port to listen (default 22) : " updatesshport
-sudo sed -i "s/#Port 22/Port ${updatesshport}/g" /etc/ssh/sshd_config
+sed -i "s/#Port 22/Port ${updatesshport}/g" /etc/ssh/sshd_config
 read -n 1 -s -r -p "Press any key to restart SSH service."
-sudo service ssh restart
+service ssh restart
 
 #ufw config
-sudo ufw default deny
-sudo ufw allow 80
-sudo ufw allow 443
-sudo ufw allow ${updatesshport}
+ufw default deny
+ufw allow 80
+ufw allow 443
+ufw allow ${updatesshport}
 read -n 1 -s -r -p "Press any key to enable ufw."
-sudo ufw enable
+ufw enable
 
 #Upgrading SSL key from 1048 bits to 4096 bits
-sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
-sudo chmod 600 /etc/ssl/certs/dhparam.pem
-sudo sed -i 's/ssl_dhparam \/etc\/letsencrypt\/ssl-dhparams.pem; # managed by Certbot/ssl_dhparam \/etc\/ssl\/certs\/dhparam.pem;/g'  /etc/nginx/sites-available/nextcloud.conf
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
+chmod 600 /etc/ssl/certs/dhparam.pem
+sed -i 's/ssl_dhparam \/etc\/letsencrypt\/ssl-dhparams.pem; # managed by Certbot/ssl_dhparam \/etc\/ssl\/certs\/dhparam.pem;/g'  /etc/nginx/sites-available/nextcloud.conf
 
 #PhpMyAdmin Virtual Host
-#sudo apt install phpmyadmin
+#apt install phpmyadmin
 echo 'server {
         root /usr/share/phpmyadmin/;
         index index.html index.htm index.php ;
